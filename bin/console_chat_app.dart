893cb7531f1f8136b199ae7610f42bd5c396a681
Boburbek6010 <boburbekkhusanboev@gmail.com>
2023-11-27 11:51:50 +0500

    first commit

---
+++ bin/console_chat_app.dart
@@ -0,0 +1,5 @@
+import 'package:console_chat_app/console_chat_app.dart' as console_chat_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${console_chat_app.calculate()}!');
+}
---
+++ test/console_chat_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:console_chat_app/console_chat_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
