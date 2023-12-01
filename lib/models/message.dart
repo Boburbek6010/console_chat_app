42cfcc53ff2896e874fee405474b9501e94f91b3
sardorbek <odiljonovsardorbek98@gmail.com>
2023-12-01 12:09:56 +0500

    time sent added to message model

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -2,14 +2,18 @@ class Message {
   late String from;
   late int to;
   late String text;
+  late DateTime timeSent;
 
-  Message(this.from, this.to, this.text);
+  Message(this.from, this.to, this.text) {
+    this.timeSent = DateTime.now();
+  }
 
   Message.fromJson(Map<String, dynamic> json) {
     from = json["from"];
     to = json["to"];
     text = json["text"];
+    timeSent = DateTime.parse(json["timeSent"]);
   }
 
-  Map<String, dynamic> toJson() => {"from": from, "to": to, "text": text};
+  Map<String, dynamic> toJson() => {"from": from, "to": to, "text": text, "timeSent": timeSent.toIso8601String()};
 }
