3fb696447efa6df6a61cc42353856fc791ad37bf
Donyor <shuxratovdoni@gmail.com>
2023-12-01 23:53:49 +0500

    Doniyor, 6-commit,the stucture of recognition user is created

--- lib/menus/login__menu.dart
+++ lib/menus/login__menu.dart
@@ -1,30 +1,37 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:console_chat_app/menus/auth__menu.dart';
 import 'package:console_chat_app/models/user.dart';
+import 'package:console_chat_app/service/network__service.dart';
 
 class Login extends Authentication {
   static const id = '/login__menu';
 
   @override
   build() async {
+    String apiEndpoint = NetworkService.apiUser;
+    String data = await NetworkService.getData(apiEndpoint);
+
     print('Enter your data:\n');
 
     stdout.write('Nick name: ');
-    String checkNickName = prompt('Enter your Nick name: ');
+    String checkNickName = prompt('');
 
-    stdout.write('Password: ');
-    String checkPassword = prompt('Enter your Nick password: ');
+    List<User> users = (json.decode(data) as List).map((json) => User.fromJson(json)).toList();
+    print('\nList of Users:');
 
-    if((checkNickName || checkPassword)  == User(nickName, password)){
-      print('equal');
+    for (User user in users) {
+      if(checkNickName == user.nickName) {
+        print('Your logged!');
+      } else {
+        print('no user founded');
+      }
     }
-    
-
   }
 
-  
   String prompt(String promptMessage) {
     stdout.write(promptMessage);
     return stdin.readLineSync()!;
   }
 }
+
